a3b066fbc8ecde8700ea5d8e476423e93390105b
robertnperalta <47515219+robertnperalta@users.noreply.github.com>
2021-11-24 10:58:58 -0800

    Add example code

---
+++ camera/example.py
@@ -0,0 +1,30 @@
+from overhead import Overhead
+from random import randint
+
+def main():
+    '''An example of using the Overhead class.'''
+    overhead = Overhead(200)
+    # Ideally, this script would wait for user confirmation that the arena and
+    # camera are set up before calling setup()
+    overhead.setup()
+
+    target = newTarget(overhead.nDots, -1)
+    while True:
+        inBoundary, gotTarget = overhead.loop(target)
+        if not inBoundary:
+            # Lose a life and stop the car or some other penalty
+            pass
+        if gotTarget:
+            target = newTarget(overhead.nDots, target)
+            # Award points, increase car speed, etc.
+
+def newTarget(nDots, lastTarget):
+    '''Returns a new target dot index. Makes sure that the same dot is not
+    chosen twice.'''
+    target = randint(0, nDots - 1)
+    while target == lastTarget:
+        target = randint(0, nDots - 1)
+    return target
+
+if __name__ == "__main__":
+    main()
--- camera/test.py
+++
@@ -1,7 +0,0 @@
-from overhead import Overhead
-
-def main():
-    overhead = Overhead()
-
-if __name__ == "__main__":
-    main()
