62a792318e5f83a351c7d83af4bd17b979cb477d
Derrick Huynh <58054564+derrickkhuynh@users.noreply.github.com>
2022-02-01 21:09:21 -0800

    website has been css"d

--- website/backend/app.js
+++ website/backend/app.js
@@ -5,7 +5,7 @@ const app = express();
 const mysql = require('mysql');
 
 const PORT = 3001;
-
+//mess with my data. I dare u >:)
 const db = mysql.createPool({
     host: "us-cdbr-east-05.cleardb.net",
     user: "bab8d04d058b12",
@@ -31,7 +31,6 @@ app.get("/api/get", (req, res) => {
 app.post("/api/user", (req, res) => {
     const username = req.body.username;
     const sql_select_user = "SELECT * FROM game_data WHERE username = " + '"' + username + '" ORDER BY score DESC, datetimestamp DESC, time_in_game DESC, powerups_used DESC, username ASC'
-    //const trivial_sql_insert = "INSERT INTO game_data VALUES (7,?,0,0,0,0)";
     db.query(sql_select_user, (err, result) => {
         res.send(result);
     });
@@ -41,11 +40,11 @@ app.post("/api/insert", (req, res) => {
     const username = req.body.username;
     const score = req.body.score;
     const powerups_used = req.body.powerups_used;
-    const datetimestamp = req.body.datetimestamp;
+    const num_turns = req.body.num_turns;
     const time_in_game = req.body.time_in_game;
-    const sql_insert_user = "INSERT INTO game_data VALUES (NULL,?,?,?,?,?)";
-    db.query(sql_insert_user, [username, score, time_in_game , datetimestamp , powerups_used], (err,result) => {
-        res.send(err);
+    const datetimestamp = req.body.datetimestamp;
+    const sql_insert_user = "INSERT INTO game_data VALUES (NULL,?,?,?,?,?,?)";
+    db.query(sql_insert_user, [username, score,  powerups_used, num_turns, time_in_game , datetimestamp], (err,result) => {
         res.send(result);
     })
 });
